2d6ab5cb5fd6b3f0d4d49189f4a8fb70553b6d82
LauroReyesSony <lauro.reyes@sonymusic.com>
2025-10-22 15:28:35 -0600

    removing admin report

--- IP_Experiment/actual_exp_phase_1/__init__.py
+++ IP_Experiment/actual_exp_phase_1/__init__.py
@@ -159,9 +159,6 @@ def creating_session(subsession: Subsession):
         p.treatment_idx = trt_idx
 
 
-def vars_for_admin_report(subsession):
-    return dict(mensaje="Dummy: pagos calculados 🚀")
-
 class Group(BaseGroup):
     pass
 
